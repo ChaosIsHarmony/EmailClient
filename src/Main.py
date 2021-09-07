6d2e10b612cc5ffec5876650af4067b2b3b708f6
EC <chaosisharmonyofficial@gmail.com>
2021-09-07 09:13:01 +0800

    added filtering by email address

--- src/Main.py
+++ src/Main.py
@@ -1,17 +1,23 @@
 from datetime import date, datetime
 import os
+from typing import List, Tuple
 
 from .EmailMessage import EmailMessage
 from .ReceivingEmail import ReceivingEmail
 from .SendingEmail import SendingEmail
 
+# determine path to credentials file
 fileDir = os.path.dirname(os.path.realpath('__file__'))
 creds_file = os.path.join(fileDir, 'misc/creds.ps')
 creds_file = os.path.abspath(os.path.realpath(creds_file))
 
+# determine path to email filter
+filter_file = os.path.join(fileDir, 'misc/filter.ps')
+filter_file = os.path.abspath(os.path.realpath(filter_file))
+
 
 def receive_email(username: str, password: str) -> None:
-    receiveEmail = ReceivingEmail()
+    receiveEmail = ReceivingEmail(get_filter())
     # login
     status = receiveEmail.connect(username, password)
     print(status)
@@ -47,18 +53,33 @@ def send_email(username: str, password: str) -> None:
     sendEmail = SendingEmail()
     status = sendEmail.connect(username, password)
     print(status)
+    # TODO: implement method
     sendEmail.disconnect()
 
 
 
+def get_filter() -> List[str]:
+    with open(filter_file, 'r') as f:
+        filter_vals = f.read().splitlines()
 
-if __name__ == "__main__":
-    # READ IN CREDENTIALS
+    return filter_vals
+
+
+
+def get_credentials() -> Tuple[str]:
     with open(creds_file, 'r') as f:
         creds = f.read().splitlines()
 
     username = creds[0]#input("Username: ")
     password = creds[1]#input("Password: ")
+
+    return username, password
+
+
+
+if __name__ == "__main__":
+    # READ IN CREDENTIALS
+    username, password = get_credentials()
 
     # DETERMINE USE CASE
     use = None
--- src/ReceivingEmail.py
+++ src/ReceivingEmail.py
@@ -8,10 +8,11 @@ from . import EmailParser
 
 class ReceivingEmail:
 
-    def __init__(self):
+    def __init__(self, filter_vals: List[str]):
         self.imapObj = imapclient.IMAPClient("imap.gmail.com", ssl=True)
         self.emails = []
         self.pp = pprint.PrettyPrinter(indent='2')
+        self.filter_vals = filter_vals
 
 
     def connect(self, username: str, password: str) -> str:
@@ -23,7 +24,10 @@ class ReceivingEmail:
         self.imapObj.select_folder("INBOX", readonly=True)
         UIDs = self.imapObj.search(["UNSEEN"])
         for UID in UIDs:
-            self.emails.append(EmailParser.parse_raw_message(UID, self.imapObj.fetch([UID], ["BODY[]", "FLAGS"])[UID]))
+            email = EmailParser.parse_raw_message(UID, self.imapObj.fetch([UID], ["BODY[]", "FLAGS"])[UID])
+            for address in self.filter_vals:
+                if address in email.get_sender():
+                    self.emails.append(email)
 
         return self.emails
 
